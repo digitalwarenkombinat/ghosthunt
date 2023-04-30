0fdf56270e7577344561adab2ff5ec3e465676ca
Gerd Müller <gerdesque@posteo.org>
2023-04-30 20:03:49 +0200

    add PWA manifest and service worker

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# ghosthunt
+# Ghost Hunt
+
+> A scavenger hunt progressive web application created with Twine and Mapbox GL JS.
---
+++ dist/service-worker.js
@@ -0,0 +1,36 @@
+// Files to cache
+const cacheName = 'ghosthunt-v1';
+const contentToCache = [
+  '/index.html'
+];
+
+// Installing Service Worker
+self.addEventListener('install', (e) => {
+  console.log('[Service Worker] Install');
+  e.waitUntil((async () => {
+    const cache = await caches.open(cacheName);
+    console.log('[Service Worker] Caching all: app shell and content');
+    await cache.addAll(contentToCache);
+  })());
+});
+
+// Fetching content using Service Worker
+self.addEventListener('fetch', (e) => {
+    // Cache http and https only, skip unsupported chrome-extension:// and file://...
+    if (!(
+       e.request.url.startsWith('http:') || e.request.url.startsWith('https:')
+    )) {
+        return; 
+    }
+
+  e.respondWith((async () => {
+    const r = await caches.match(e.request);
+    console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
+    if (r) return r;
+    const response = await fetch(e.request);
+    const cache = await caches.open(cacheName);
+    console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
+    cache.put(e.request, response.clone());
+    return response;
+  })());
+});
