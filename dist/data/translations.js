45eccdcf88d67771c6f4db7037d00972d40e9517
Gerd Müller <gerdesque@posteo.org>
2023-09-15 18:04:47 +0200

    Refactor twee structure for v0.2

--- dist/data/translations.js
+++ dist/data/translations.js
@@ -70,5 +70,61 @@ getTranslations = function () {
       en: 'Legal Notice'
     }
   }
+  text["Inventar"] = {
+    "1": {
+      de: 'Geister',
+      en: 'Ghosts'
+    },
+    "2": {
+      de: 'Hier werden alle Geister angezeigt, die du mit der AR-Kamera eingefangen hast.',
+      en: 'All the ghosts you captured with the AR camera are displayed here.'
+    },
+    "3": {
+      de: 'AR-Kamera öffnen',
+      en: 'Open AR camera'
+    }
+  }
+  text["Hilfe"] = {
+    "1": {
+      de: 'Hilfe',
+      en: 'Help'
+    },
+    "2": {
+      de: 'Erkunde die Stationen auf der Karte, um geheime Geister zu finden und Buchstaben für das Lösungswort zu sammeln.',
+      en: 'Explore the stations on the map to find secret ghosts and collect and collect letters for the solution word.'
+    },
+    "3": {
+      de: 'Wo finde ich meinen Spielfortschritt?',
+      en: 'Where can I find my game progress?'
+    },
+    "4": {
+      de: 'Deinen Spielfortschritt kannst du hier einsehen:',
+      en: 'You can check your game progress here:'
+    },
+    "5": {
+      de: 'Was passiert, wenn ich das Lösungswort vollständig erraten habe?',
+      en: 'What happens when I have completely guessed the solution word?'
+    },
+    "6": {
+      de: 'Sed non urna. Donec et ante. Phasellus eu ligula. Vestibulum sit amet purus.',
+      en: 'Sed non urna. Donec et ante. Phasellus eu ligula. Vestibulum sit amet purus.'
+    },
+    "7": {
+      de: 'Benötigt jeder Mitspielende ein eigenes Handy?',
+      en: 'Does each player need their own cell phone?'
+    },
+    "8": {
+      de: 'Wir empfehlen das Spiel als Gruppe mit einem Handy zu spielen. Natürlich könnte ihr aber auch jeder alleine mit einem Handy losziehen. Es gibt keine vorgeschriebene Reihenfolge für die Stationen. Am Ende könnt ihr vergleichen, wer mehr Punkte und Geister gesammelt hat.',
+      en: 'We recommend playing the game as a group with a cell phone. Of course, you could also each go off on your own with a cell phone. There is no prescribed order for the stations. At the end you can compare who has collected more points and ghosts.'
+    },
+    "9": {
+      de: 'Wie kann ich die Rätsel lösen?',
+      en: 'How can I solve the puzzles?'
+    },
+    "10": {
+      de: 'Die Rätsel im Spiel können nur durch genaues Absuchen eurer Umgebung gelöst werden. Findet die Orte auf dem Bild und studiert sie genau. Dann findet ihr die Antwort.',
+      en: 'The puzzles in the game can only be solved by carefully scanning your surroundings. Find the places on the picture and study them carefully. Then you will find the answer.'
+    },
+  }
   return text;
 };
