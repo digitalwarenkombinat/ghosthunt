bd2509dc5f2b1d26075e0b2b01b7334808ebcaa3
Gregor on Mac <gregorassfalg@gmail.com>
2023-10-19 13:34:43 +0200

    alte assets gelöscht und service worker geupdarted

--- dist/service-worker.js
+++ dist/service-worker.js
@@ -1,8 +1,60 @@
 // Files to cache
-const cacheName = 'ghosthunt-v1';
-const contentToCache = [
-  '/index.html'
+const cacheName = 'ghosthunt-v0.1';
+const contentToCacheiOS = [
+  'index.html',
+  'images/bg.jpg',
+  'images/menu.jpg',
+  'fonts/Creepster/Creepster-Regular.ttf',
+  'fonts/para.otf',
+  'images/icon.png',
+  'videos/intro.1.mp4',
+  'models/setup.3/setuup.3.usdz',
+  'audio/dist/audio/Voices, Laugh, Male, Mysterious Laughter SND67498.wav',
+  'images/holger.png',
+  'images/holger.bg.jpg',
+  'videos/intro.2.mp4',
+  'videos/intro.3.mp4',
+  'videos/intro.4.mp4',
+  'videos/intro.6.mp4',
+  'images/paranormal.jpg',
+  'images/cam.jpg',
+  'videos/1.1.mp4',
+  'models/kapitel.1/kapitel.1.usdz',
+  'videos/1.3.mp4',
+  'videos/1.5.mp4',
+  'models/herbert/herbert.usdz',
+  'models/kapitel.2/kapitel.2.usdz',
+  'models/rosa/rosa.usdz'
 ];
+
+const urlsToCacheAndroid = [ 
+  'index.html',
+  'images/bg.jpg',
+  'images/menu.jpg',
+  'fonts/Creepster/Creepster-Regular.ttf',
+  'fonts/para.otf',
+  'images/icon.png',
+  'videos/intro.1.mp4',
+  'models/setup.3/setuup.3.glb',
+  'audio/dist/audio/Voices, Laugh, Male, Mysterious Laughter SND67498.wav',
+  'images/holger.png',
+  'images/holger.bg.jpg',
+  'videos/intro.2.mp4',
+  'videos/intro.3.mp4',
+  'videos/intro.4.mp4',
+  'videos/intro.6.mp4',
+  'images/paranormal.jpg',
+  'images/cam.jpg',
+  'videos/1.1.mp4',
+  'models/kapitel.1/kapitel.1.glb',
+  'videos/1.3.mp4',
+  'videos/1.5.mp4',
+  'models/herbert/herbert.glb',
+  'models/kapitel.2/kapitel.2.glb',
+  'models/rosa/rosa.glb'
+  ];
+
+const self = this;
 
 // Installing Service Worker
 self.addEventListener('install', (e) => {
