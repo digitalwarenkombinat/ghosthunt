80ddcfc591dae88a8fe5dd9678fafd6f68129167
gerdesque <gerdesque@users.noreply.github.com>
2023-10-21 14:09:16 +0000

    Deploying to gh-pages from @ digitalwarenkombinat/ghosthunt@b6521c5cd2f692cf1fa6545432f8d71902711e47 🚀

--- service-worker.js
+++ service-worker.js
@@ -1,99 +1,103 @@
-// Files to cache
 const cacheName = "ghosthunt-v0.1";
 const urlsToCacheiOS = [
+  '/',
   'index.html',
-  'images/bg.jpg',
-  'images/menu.jpg',
   'fonts/Creepster/Creepster-Regular.ttf',
   'fonts/para.otf',
-  'images/icon.png',
-  'videos/intro.1.mp4',
-  'models/setup.3/setuup.3.usdz',
-  'audio/dist/audio/Voices, Laugh, Male, Mysterious Laughter SND67498.wav',
-  'images/holger.png',
+  'images/bg.jpg',
+  'images/cam.jpg',
   'images/holger.bg.jpg',
+  'images/holger.png',
+  'images/icon.png',
+  'images/menu.jpg',
+  'images/paranormal.jpg',
+  'models/herbert/herbert.usdz',
+  'models/kapitel.1/kapitel.1.usdz',
+  'models/kapitel.2/kapitel.2.usdz',
+  'models/rosa/rosa.usdz',
+  'models/setup.3/setup.3.usdz',
+  'videos/1.1.mp4',
+  'videos/1.3.mp4',
+  'videos/1.5.mp4',
+  'videos/intro.1.mp4',
   'videos/intro.2.mp4',
   'videos/intro.3.mp4',
   'videos/intro.4.mp4',
   'videos/intro.6.mp4',
-  'images/paranormal.jpg',
-  'images/cam.jpg',
-  'videos/1.1.mp4',
-  'models/kapitel.1/kapitel.1.usdz',
-  'videos/1.3.mp4',
-  'videos/1.5.mp4',
-  'models/herbert/herbert.usdz',
-  'models/kapitel.2/kapitel.2.usdz',
-  'models/rosa/rosa.usdz'
 ];
 
-const urlsToCacheAndroid = [ 
+const urlsToCacheAndroid = [
+  '/',
   'index.html',
-  'images/bg.jpg',
-  'images/menu.jpg',
   'fonts/Creepster/Creepster-Regular.ttf',
   'fonts/para.otf',
-  'images/icon.png',
-  'videos/intro.1.mp4',
-  'models/setup.3/setuup.3.glb',
-  'audio/dist/audio/Voices, Laugh, Male, Mysterious Laughter SND67498.wav',
-  'images/holger.png',
+  'images/bg.jpg',
+  'images/cam.jpg',
   'images/holger.bg.jpg',
+  'images/holger.png',
+  'images/icon.png',
+  'images/menu.jpg',
+  'images/paranormal.jpg',
+  'models/herbert/herbert.glb',
+  'models/kapitel.1/kapitel.1.glb',
+  'models/kapitel.2/kapitel.2.glb',
+  'models/rosa/rosa.glb',
+  'models/setup.3/setup.3.glb',
+  'videos/1.1.mp4',
+  'videos/1.3.mp4',
+  'videos/1.5.mp4',
+  'videos/intro.1.mp4',
   'videos/intro.2.mp4',
   'videos/intro.3.mp4',
   'videos/intro.4.mp4',
   'videos/intro.6.mp4',
-  'images/paranormal.jpg',
-  'images/cam.jpg',
-  'videos/1.1.mp4',
-  'models/kapitel.1/kapitel.1.glb',
-  'videos/1.3.mp4',
-  'videos/1.5.mp4',
-  'models/herbert/herbert.glb',
-  'models/kapitel.2/kapitel.2.glb',
-  'models/rosa/rosa.glb'
   ];
 
   const self = this;
 
-  // Install SW
-  self.addEventListener('install', (event) => {
-      event.waitUntil(
-          caches.open(CACHE_NAME)
-              .then((cache) => {
-                  console.log('Opened cache');
+  // Populating your cache
+  const addResourcesToCache = async () => {
+    const cache = await caches.open(cacheName);
+    const is_safari = navigator.userAgent.indexOf('Safari') > -1 && navigator.userAgent.indexOf('Chrome') <= -1;
+    await cache.addAll(is_safari ? urlsToCacheiOS : urlsToCacheAndroid);
+  };
   
-                  const is_safari = navigator.userAgent.indexOf('Safari') > -1 && navigator.userAgent.indexOf('Chrome') <= -1;
+  const putInCache = async (request, response) => {
+    const cache = await caches.open(cacheName);
+    await cache.put(request, response);
+  };
   
-                  return cache.addAll(is_safari ? urlsToCacheiOS : urlsToCacheAndroid);
-              })
-      )
+  const cacheFirst = async (request) => {
+    const responseFromCache = await caches.match(request);
+    if (responseFromCache) {
+      return responseFromCache;
+    }
+    const responseFromNetwork = await fetch(request);
+    putInCache(request, responseFromNetwork.clone());
+    return responseFromNetwork;
+  };
+ 
+  // Enable navigation preload
+  const enableNavigationPreload = async () => {
+    if (self.registration.navigationPreload) {
+      await self.registration.navigationPreload.enable();
+    }
+  };
+
+  // Activate service worker
+  self.addEventListener("activate", (event) => {
+    event.waitUntil(enableNavigationPreload());
   });
-  
-  // Listen for requests
-  self.addEventListener('fetch', (event) => {
-      event.respondWith(
-          caches.match(event.request)
-              .then(() => {
-                  return fetch(event.request)
-                      .catch(() => caches.match('offline.html'))
-              })
-      )
+
+  // Install service worker
+  self.addEventListener("install", (event) => {
+    event.waitUntil(
+      addResourcesToCache(),
+    );
   });
-  
-  // Activate the SW
-  self.addEventListener('activate', (event) => {
-      const cacheWhiteList =[];
-      cacheWhiteList.push(CACHE_NAME);
-  
-      event.waitUntil(
-          caches.keys().then((cacheName) => Promise.all(
-               cacheName.map((cacheName) => {
-                   if(!cacheWhiteList.includes(cacheName)) {
-                       return caches.delete(cacheName);
-                   }
-               })
-          ))
-      )
+
+  // Custom responses to requests
+  self.addEventListener("fetch", (event) => {
+    event.respondWith(cacheFirst(event.request));
   });
   
