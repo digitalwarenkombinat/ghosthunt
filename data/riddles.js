08fd4d4f8b176e909c7796651a5dce601b06bb63
macgregger <macgregger27@gmail.com>
2024-01-16 12:15:15 +0000

    Deploying to gh-pages from @ digitalwarenkombinat/ghosthunt@228bac26b119408c72b8ca266edf0d6a7d629e8c 🚀

--- data/riddles.js
+++ data/riddles.js
@@ -49,57 +49,57 @@ getRiddles = function () {
     [
       {
         question: {
-          de: "Wer war gestorben?",
-          en: "Who died?"
+          de: "Wie viele Guhle zählst du?",
+          en: "How many Gouhls do you count?"
         },
         answers: [
-          { de: "Rose Horwitz", en: "Rose Horwitz", correct: false, points: 0 },
-          { de: "Rosa Herman", en: "Rosa Herman", correct: true, points: 10 },
-          { de: "Jose Hetman", en: "Jose Hetman", correct: false, points: 0 }
+          { de: "1", en: "1", correct: false, points: 0 },
+          { de: "3", en: "3", correct: true, points: 10 },
+          { de: "2", en: "2", correct: false, points: 0 }
         ]
       },
       {
         question: {
-          de: "Was war passiert?",
-          en: "What had happened?"
+          de: "Was ist nicht im Kühlschrank?",
+          en: "What is not in the fridge?"
         },
         answers: [
-          { de: "Opferritual", en: "Ritual of sacrifice", correct: false, points: 0 },
-          { de: "Beerdigung", en: "Funeral", correct: false, points: 0 },
-          { de: "Grabraub", en: "Grave robbery", correct: true, points: 10 }
+          { de: "Arme", en: "arms", correct: false, points: 0 },
+          { de: "Füße", en: "feet", correct: false, points: 0 },
+          { de: "Köpfe", en: "heads", correct: true, points: 10 }
         ]
       },
       {
         question: {
-          de: "Wie viele Räuber waren es?",
-          en: "How many robbers were there?"
+          de: "Wie viele Dosen gibt es?",
+          en: "How many cans are there?"
         },
         answers: [
-          { de: "Einer", en: "One", correct: false, points: 0 },
-          { de: "Zwei", en: "Two", correct: true, points: 10 },
-          { de: "Drei", en: "Three", correct: false, points: 0 }
+          { de: "7", en: "7", correct: false, points: 0 },
+          { de: "8", en: "8", correct: true, points: 10 },
+          { de: "9", en: "9", correct: false, points: 0 }
         ]
       },
       {
         question: {
-          de: "Was haben die Räuber gestohlen?",
-          en: "What did the robbers steal?"
+          de: "Was ist nicht in der Szene?",
+          en: "What is not in the scene?"
         },
         answers: [
-          { de: "Bier", en: "Beer", correct: false, points: 0 },
-          { de: "Ringe", en: "Rings", correct: true, points: 10 },
-          { de: "Schal", en: "Scarf", correct: false, points: 0 }
+          { de: "Gläser", en: "glases", correct: false, points: 0 },
+          { de: "Beil", en: "hatchet", correct: true, points: 10 },
+          { de: "Dosen", en: "cans", correct: false, points: 0 }
         ]
       },
       {
         question: {
-          de: "Gibt es Hinweise auf die Identität der Räuber?",
-          en: "Are there clues to the identity of the robbers?"
+          de: "Was hat ein Guhl in der Hand?",
+          en: "What's in the hand of one ghoul?"
         },
         answers: [
-          { de: "Ausweis", en: "ID card", correct: true, points: 10 },
-          { de: "Brechstange", en: "Crowbar", correct: false, points: 0 },
-          { de: "Axt", en: "Axe", correct: false, points: 0 }
+          { de: "Flasche", en: "bottle", correct: true, points: 10 },
+          { de: "Kopf", en: "Head", correct: false, points: 0 },
+          { de: "Tasse", en: "Mug", correct: false, points: 0 }
         ]
       }
     ],
